436a83115a473e0f6187ed26d29c00da44ff0d74
Roksolana Kalinska <woops359@gmail.com>
2025-05-15 17:22:59 +0300

    Added reservations.urls

--- reservations/urls.py
+++ reservations/urls.py
@@ -1,9 +1,16 @@
-from django.urls import path, include
-from rest_framework import routers
+from django.urls import path
 
+from reservations.views import AdminReservationListView, CreateReservationView, MyReservationDeleteView, \
+    MyReservationDetailView, \
+    MyReservationListView
 
 app_name = "cinema"
 
-router = routers.DefaultRouter()
 
-urlpatterns = [path("", include(router.urls))]
+urlpatterns = [
+    path("create-reservation/", CreateReservationView.as_view(), name="create_reservation"),
+    path("my-reservations/", MyReservationListView.as_view(), name="my_reservations"),
+    path("my-reservations/<int:pk>/", MyReservationDetailView.as_view(), name="my_reservation_detail"),
+    path("my-reservations/<int:pk>/delete/", MyReservationDeleteView.as_view(), name="my_reservation_delete"),
+    path("all-reservations/", AdminReservationListView.as_view(), name="all_reservations"),
+]
