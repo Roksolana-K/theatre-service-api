2f3c1b06fa7163146416d468a44b89a5fc8a5f59
Roksolana Kalinska <woops359@gmail.com>
2025-05-15 11:03:10 +0300

    Added CRUD for user

--- user/views.py
+++ user/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-# Create your views here.
+from user.serializers import UserCreateSerializer, UserDetailSerializer, UserUpdateSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserCreateSerializer
+    permission_classes = [AllowAny]
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_serializer_class(self):
+        if self.request.method in ["PUT", "PATCH"]:
+            return UserUpdateSerializer
+        return UserDetailSerializer
+
+    def get_object(self):
+        return self.request.user
+
+
+class DeleteUserView(generics.DestroyAPIView):
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
