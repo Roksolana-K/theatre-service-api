fecd1e35108a3ddf6cff3b0473bc406298e6e112
Roksolana Kalinska <woops359@gmail.com>
2025-05-18 18:32:54 +0300

    Changed the name of urls, because it was confusing

--- reservations/urls.py
+++ reservations/urls.py
@@ -4,13 +4,13 @@ from reservations.views import AdminReservationListView, CreateReservationView, 
     MyReservationDetailView, \
     MyReservationListView
 
-app_name = "cinema"
+app_name = "reservations"
 
 
 urlpatterns = [
-    path("create-reservation/", CreateReservationView.as_view(), name="create_reservation"),
-    path("my-reservations/", MyReservationListView.as_view(), name="my_reservations"),
-    path("my-reservations/<int:pk>/", MyReservationDetailView.as_view(), name="my_reservation_detail"),
-    path("my-reservations/<int:pk>/delete/", MyReservationDeleteView.as_view(), name="my_reservation_delete"),
-    path("all-reservations/", AdminReservationListView.as_view(), name="all_reservations"),
+    path("create/", CreateReservationView.as_view(), name="create_reservation"),
+    path("my/", MyReservationListView.as_view(), name="my_reservations"),
+    path("my/<int:pk>/", MyReservationDetailView.as_view(), name="my_reservation_detail"),
+    path("my/<int:pk>/delete/", MyReservationDeleteView.as_view(), name="my_reservation_delete"),
+    path("all/", AdminReservationListView.as_view(), name="all_reservations"),
 ]
